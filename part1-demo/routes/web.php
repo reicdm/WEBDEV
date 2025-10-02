b5c7bc29149efd05e5e099627234406dd54979f4
reicdm <reicdmagpantay@gmail.com>
2025-10-02 19:59:43 +0800

    routes and blade

--- part1-demo/routes/web.php
+++ part1-demo/routes/web.php
@@ -6,11 +6,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user', function(){
-    return "<h1>Reina Chloe Magpantay</h1>";
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', function(){
+        return view('Users.user');
+    })->name("userName");
+
+    Route::get('/{id}', function($id){
+        // return "Your ID is: " . $id;
+        return "<a href= '".route('editUser', $id)."'>Edit</a>";
+    });
+
+    Route::get('/{id}/{name}', function($id, $name) {
+        return "Your ID is: " . $id . " " . $name;
+    });
 });
+
+Route::get('/edit/{id}', function($id) {
+    return "<a href='".route('homeUser', $id)."'>Home</a>";
+})->name("editUser");
+
+Route::get('/home/{id}', function($id){
+    return "Welcome " . $id;
+})->name("homeUser");
+
 
 Route::get('/admin/edit', function(){
     return "<h1>Employee Page</h1>";
 });
 
+
+Route::fallback(function(){
+    return redirect()->route('userName');
+});
