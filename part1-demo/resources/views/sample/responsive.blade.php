c3c1eb178005a48708633feb45497203ed5cfa8d
reicdm <reicdmagpantay@gmail.com>
2025-10-13 20:38:19 +0800

    grid, append -> main

---
+++ part1-demo/resources/views/sample/responsive.blade.php
@@ -0,0 +1,172 @@
+@extends('layout.main')
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-3 border bg-success">Left</div>
+        <div class="col-lg-3 col-md-6 col-sm-3 border bg-primary">Middle</div>
+        <div class="col-lg-3 col-md-6 col-sm-3 border bg-danger">Right</div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-8">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-8">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>@social</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 p-1">
+            <div class="card shadow">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endSection
--- part1-demo/routes/web.php
+++ part1-demo/routes/web.php
@@ -6,39 +6,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'user'], function(){
-    Route::get('/', function(){
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', function () {
         return view('Users.user');
     })->name("userName");
 
-    Route::get('/{id}', function($id){
+    Route::get('/{id}', function ($id) {
         // return "Your ID is: " . $id;
-        return "<a href= '".route('editUser', $id)."'>Edit</a>";
+        return "<a href= '" . route('editUser', $id) . "'>Edit</a>";
     });
 
-    Route::get('/{id}/{name}', function($id, $name) {
+    Route::get('/{id}/{name}', function ($id, $name) {
         return "Your ID is: " . $id . " " . $name;
     });
 });
 
-Route::get('/edit/{id}', function($id) {
-    return "<a href='".route('homeUser', $id)."'>Home</a>";
+Route::get('/edit/{id}', function ($id) {
+    return "<a href='" . route('homeUser', $id) . "'>Home</a>";
 })->name("editUser");
 
-Route::get('/home/{id}', function($id){
+Route::get('/home/{id}', function ($id) {
     return "Welcome " . $id;
 })->name("homeUser");
 
 
-Route::get('/admin/edit', function(){
+Route::get('/admin/edit', function () {
     return "<h1>Employee Page</h1>";
 });
 
-
-Route::fallback(function(){
-    return view('error');
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
 
-Route::get('/dashboard', function(){
-    return view('dashboard');
+Route::get('/responsive', function () {
+    return view('sample.responsive');
+});
+
+Route::fallback(function () {
+    return view('error');
 });
